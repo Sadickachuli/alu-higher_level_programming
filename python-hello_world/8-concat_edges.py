fb0818b72c19564ddf58ec778248858a23a9069e
Sadickachuli <m.achuli@alustudent.com>
2023-02-04 01:41:18 -0800

    concat 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str = str[38:67] + str[106:112] + str[0:6]
+str = str[38:67].strip() + str[106:112] + str[0:6].strip()
 print(str)
