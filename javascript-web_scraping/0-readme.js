6eb472cd609ed685fe4585c274f99f029273d146
Sadickachuli <m.achuli@alustudent.com>
2023-06-16 13:59:49 +0200

    Jsws

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-let filename = process.argv[2];
 const fs = require('fs');
-fs.readFile(filename, 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
+const fname = process.argv[2];
+
+fs.readFile(fname, (err, content) => {
+  err ? console.log(err) : console.log(content.toString());
+}
+);
